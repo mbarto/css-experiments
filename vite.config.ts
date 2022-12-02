09c9ac2433d0571b1f92eae2a250924ae650de2b
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2022-12-02 17:48:52 +0100

    improved tab panel experiment, and added a version using the postcss-when plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import postcssWhen from "postcss-when"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  css: {
+    postcss: {
+        plugins: [postcssWhen()],
+    },
+  },
 })
