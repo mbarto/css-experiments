43056c43f58ae4010d615dc3235914bc2c732c2f
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2022-12-16 11:43:49 +0100

    fixed production build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import postcssWhen from "postcss-when"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        toggle: 'toggle.html',
+        tabs: 'tabs.html',
+        tabs2: 'tabs2.html',
+      },
+    },
+  },
   plugins: [react()],
   css: {
     postcss: {
