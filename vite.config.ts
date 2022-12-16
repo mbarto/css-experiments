ad4410164fca49df0badd8d4c01e8eade1401e93
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2022-12-16 18:48:58 +0100

    added polyfill for :has

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import postcssWhen from "postcss-when"
+import postcssHasPseudo from "css-has-pseudo"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +10,9 @@ export default defineConfig({
       input: {
         main: 'index.html',
         toggle: 'toggle.html',
+        togglepolyfill: 'toggle-polyfill.html',
         tabs: 'tabs.html',
+        tabspolyfill: 'tabs-polyfill.html',
         tabs2: 'tabs2.html',
       },
     },
@@ -17,7 +20,7 @@ export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
-        plugins: [postcssWhen()],
+        plugins: [postcssWhen(), postcssHasPseudo()],
     },
   },
 })
