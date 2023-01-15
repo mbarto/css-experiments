93a56d8c38f72b136e250a0a86362ecaea451a2c
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2023-01-15 14:03:49 +0100

    added colors and toc examples (WIP)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import postcssWhen from "postcss-when"
+import postcssFor from "postcss-for";
 import postcssHasPseudo from "css-has-pseudo"
 
 // https://vitejs.dev/config/
@@ -8,19 +9,19 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: 'index.html',
-        toggle: 'toggle.html',
-        togglepolyfill: 'toggle-polyfill.html',
-        tabs: 'tabs.html',
-        tabspolyfill: 'tabs-polyfill.html',
-        tabs2: 'tabs2.html',
+        main: "index.html",
+        toggle: "toggle.html",
+        togglepolyfill: "toggle-polyfill.html",
+        tabs: "tabs.html",
+        tabspolyfill: "tabs-polyfill.html",
+        tabs2: "tabs2.html",
       },
     },
   },
   plugins: [react()],
   css: {
     postcss: {
-        plugins: [postcssWhen(), postcssHasPseudo()],
+      plugins: [postcssWhen(), postcssHasPseudo(), postcssFor()],
     },
   },
-})
+});
