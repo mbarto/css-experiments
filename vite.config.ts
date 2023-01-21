d3a17e5c9f02eac83582f1bed5c58896a8a2ee15
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2023-01-21 14:25:19 +0100

    updated colors example to use postcss-dynamic-value plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import postcssWhen from "postcss-when"
+import postcssDynamicValue from "postcss-dynamic-value";
 import postcssFor from "postcss-for";
 import postcssHasPseudo from "css-has-pseudo"
 
@@ -24,7 +25,7 @@ export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
-      plugins: [postcssWhen(), postcssHasPseudo(), postcssFor()],
+      plugins: [postcssWhen(), postcssHasPseudo(), postcssFor(), postcssDynamicValue()],
     },
   },
 });
