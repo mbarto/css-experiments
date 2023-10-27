ab079f0c6049d2bb8b07a3660d444e59aed4bdf5
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2023-10-27 14:38:12 +0200

    chore: cleaned up package.json and updated vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import postcssWhen from "postcss-when"
+import { defineConfig } from "vite";
+import postcssWhen from "postcss-when";
 import postcssDynamicValue from "postcss-dynamic-value";
 import postcssFor from "postcss-for";
-import postcssHasPseudo from "css-has-pseudo"
+import postcssHasPseudo from "css-has-pseudo";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,7 +24,7 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [],
   css: {
     postcss: {
       plugins: [
